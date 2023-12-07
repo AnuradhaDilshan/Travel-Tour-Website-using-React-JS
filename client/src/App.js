9998d709a12c313e120cbbf0e5cffe870b405802
Anuradha Dilshan <101551005+AnuradhaDilshan@users.noreply.github.com>
2023-12-07 18:48:52 +0530

    Home Component Changes

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,7 @@
 import React from "react";
 import "./App.css";
 import Navbar from "./Components/Navbar/Navbar";
-// import Home from "./Components/Home/home";
+import Home from "./Components/Home/Home";
 // import Main from "./Components/Main/main";
 // import Footer from "./Components/Footer/Footer";
 
@@ -9,8 +9,8 @@ const App = () => {
   return (
     <>
       <Navbar />
-      {/* <Home />
-      <Main />
+      <Home />
+      {/* <Main />
       <Footer /> */}
     </>
   );
